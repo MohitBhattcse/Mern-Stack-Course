899433ccbffdd6c989da4fe17e8a34e4ef098e7e
Cipherschools <you@example.com>
2024-07-04 15:06:13 +0530

    Till Lec 14

---
+++ JS/example.js
@@ -0,0 +1,26 @@
+// var score=10;//ecma es5
+// let result=10;//ecma es6
+
+// const PI=3.14;//ecma es6
+// console.log(PI);
+// score=19;
+// result=20;
+// PI=3.57
+// console.log(score);
+// console.log(result);
+// console.log(PI);
+
+let score = 10 ;
+let result = '15';
+//Concatenation
+let finals= score+result;
+let penaly=score-result;
+let n=5;
+// let m=finals/5;
+// console.log(m);
+// // console.log(finals);
+// // console.log(penaly);
+// // // console.log(score, result)
+// // console.log(typeof finals);
+//now bodmas rule
+console.log(finals**2)*10/(penaly+finals);
